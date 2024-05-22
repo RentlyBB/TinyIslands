0aca7445f52ac620fc580d028c9d3f80767752b1
Patrik <patrikchleboun8@gmail.com>
2024-05-22 23:17:12 +0200

    camera move smoothly to the new position

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,28 +1,37 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using KinematicCharacterController.PlayerCharacter;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-    
-    public Transform target; // The target object around which the camera will rotate
+
+    private Camera _camera;
     
     [Space]    
     [Header("Zoom Settings")]
-    public float zoomSpeed = 2.0f; // Speed of zooming
+    public float zoomSpeed = 25.0f; // Speed of zooming
     public float minZoomSize = 1.0f; // Minimum orthographic size (zoom out limit)
-    public float maxZoomSize = 10.0f; // Maximum orthographic size (zoom in limit)
+    public float maxZoomSize = 50.0f; // Maximum orthographic size (zoom in limit)
     public float zoomSmoothing = 5.0f; // Smoothing factor for zooming
-    
-    public float zOffset = -10;
 
-    private float targetZoomSize = 50;
+    [Space]
+    [Header("Camera Movement Settings")]
+    public float smoothTime = 0.3f; // Time for the smooth 
+    public float zOffset = -100;
+
+    private Vector3 _velocity = Vector3.zero;
+    private float _targetZoomSize = 50;
+    private Vector3 _targetPosition;
+
+    private void Start() {
+        _camera = GetComponent<CharacterCamera>().Camera;
+    }
 
     private void LateUpdate() {
-        
         ZoomCamera();
-
+        transform.position = Vector3.SmoothDamp(transform.position, _targetPosition, ref _velocity, smoothTime);
     }
 
 
@@ -30,20 +39,14 @@ public class CameraController : MonoBehaviour {
         float zoomInput = Input.GetAxis("Mouse ScrollWheel");
         if (zoomInput != 0) {
             // Calculate the target orthographic size based on the zoom input
-            targetZoomSize = Mathf.Clamp(targetZoomSize - zoomInput * zoomSpeed, minZoomSize, maxZoomSize);
+            _targetZoomSize = Mathf.Clamp(_targetZoomSize - zoomInput * zoomSpeed, minZoomSize, maxZoomSize);
         }
 
         // Smoothly interpolate towards the target orthographic size
-        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, targetZoomSize, zoomSmoothing * Time.deltaTime);
+        _camera.orthographicSize = Mathf.Lerp(_camera.orthographicSize, _targetZoomSize, zoomSmoothing * Time.deltaTime);
     }
 
-    //Localy can move on X and Y axes
-    public void MoveCameraTo(Vector3 targetPos) {
-        transform.position = targetPos;
-        transform.localPosition +=  transform.TransformDirection (new Vector3(0, 0, zOffset));
-
-        Debug.Log("Receive position: " + targetPos);
-        
+    public void GetCameraTargetPosition(Vector3 targetPos) {
+        _targetPosition = targetPos + transform.TransformDirection(new Vector3(0, 0, zOffset));
     }
-
 }
